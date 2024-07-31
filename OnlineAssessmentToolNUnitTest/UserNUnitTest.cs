1fabbca25b369a9db0182a83872926d9558f3530
Aswin P T <aswinpt.apt@gmail.com>
2024-07-31 10:24:02 +0530

    Added NUnit test for Auth and permission

--- OnlineAssessmentToolNUnitTest/UserNUnitTest.cs
+++ OnlineAssessmentToolNUnitTest/UserNUnitTest.cs
@@ -10,7 +10,7 @@ using System;
 using AutoMapper;
 using OnlineAssessmentTool.Repository.IRepository;
 
-namespace OnlineAssessmentToolTestProject
+namespace OnlineAssessmentToolNUnitTest
 {
     [TestFixture]
     public class UserNUnitTest
