1d4a56e0202b36bab83ca7730704ade77dc806d9
Aswin P T <aswinpt.apt@gmail.com>
2024-08-08 20:58:33 +0530

    Added smtp email client api

---
+++ OnlineAssessmentTool/Controllers/MailController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineAssessmentTool.Services.IService;
+using OnlineAssessmentTool.Models.DTO;
+
+namespace OnlineAssessmentTool.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]/[action]")]
+    public class MailController : ControllerBase
+    {
+        private readonly IEmailService _emailService;
+
+        public MailController(IEmailService emailService)
+        {
+            _emailService = emailService;
+        }
+
+        [HttpPost("SendMail")]
+        public async Task<IActionResult> SendMail([FromBody] EmailRequestDTO emailRequest)
+        {
+            await _emailService.SendEmailAsync(emailRequest.ToEmail, emailRequest.Subject, emailRequest.Body);
+            return Ok("Email sent successfully!");
+        }
+    }
+}
